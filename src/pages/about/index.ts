9966826f2095f93c653d120d24be9acc80068f23
Mulumba Nasiifu <46749403+MNasiifu@users.noreply.github.com>
2025-09-13 19:41:38 +0300

    Update index.ts

--- src/pages/about/index.ts
+++ src/pages/about/index.ts
@@ -1,4 +1,3 @@
-export { default as About } from './About';
 export { default as Team } from './Team';
 export { default as Contact } from './Contact';
 export { default as WhoWeAre } from './WhoWeAre';
